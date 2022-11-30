28593934fc5a92af98055413830387c7f300857e
Kevin K <kbknapp@gmail.com>
2022-11-29 22:09:56 -0500

    clippy

--- build.rs
+++ build.rs
@@ -2,12 +2,13 @@ use std::process::Command;
 
 fn main() {
     let output = Command::new("git")
-        .args(&["rev-parse", "HEAD"])
+        .args(["rev-parse", "HEAD"])
         .output()
         .unwrap();
     let git_hash = String::from_utf8(output.stdout).unwrap();
     println!(
-        "cargo:rustc-env=VERSION_WITH_GIT_HASH={}",
-        format!("v{} ({})", env!("CARGO_PKG_VERSION"), &git_hash[..10])
+        "cargo:rustc-env=VERSION_WITH_GIT_HASH=v{} ({})",
+        env!("CARGO_PKG_VERSION"),
+        &git_hash[..10]
     );
 }
