c4b06cc8664cc1b316a5051ee9686bd5e41d3af4
Kevin K <kbknapp@gmail.com>
2023-02-06 21:57:52 -0500

    CHECKPOINT

--- src/iptables/rule.rs
+++ src/iptables/rule.rs
@@ -6,6 +6,14 @@ use anyhow::Result;
 pub(crate) struct Rule {
     pub(crate) rule: String,
     pub(crate) counter: Counter,
+    pub(crate) comment: Option<String>,
+}
+
+macro_rules! regex {
+    ($re:literal $(,)?) => {{
+        static RE: once_cell::sync::OnceCell<regex::Regex> = once_cell::sync::OnceCell::new();
+        RE.get_or_init(|| regex::Regex::new($re).unwrap())
+    }};
 }
 
 impl Rule {
@@ -17,10 +25,22 @@ impl Rule {
         let chain_start = idx_after(flag, line, ' ').unwrap_or(0);
         let chain_end = idx_after(chain_start + 2, line, ' ').unwrap_or(line.len());
         let chain = line[chain_start..chain_end].trim().into();
+        let after_chain = line[chain_end..].trim();
+        let re = regex!(r#"--comment +"((?:[^"\\]|\\.)*)""#);
+        let re_no_quotes = regex!(r#"--comment +((?:[^ "\\]|\\.)*)"#);
+        let comment = if let Some(caps) = re.captures(after_chain) {
+            caps.get(1).map(|m| m.as_str().to_owned())
+        } else if let Some(caps) = re_no_quotes.captures(after_chain) {
+            caps.get(1).map(|m| m.as_str().to_owned())
+        } else {
+            None
+        };
+
         Ok((
             chain,
             Self {
                 rule: line[chain_end..].trim().into(),
+                comment,
                 counter,
             },
         ))
@@ -38,6 +58,7 @@ mod test {
             ("LIBVIRT_PRT".into(),
              Rule {
                 rule: "-s 192.168.122.0/24 ! -d 192.168.122.0/24 -p tcp -j MASQUERADE --to-ports 1024-65535".into(),
+                comment: None,
                 counter: Counter {
                     packets: 0,
                     bytes: 0,
@@ -54,10 +75,62 @@ mod test {
                 "POSTROUTING".into(),
                 Rule {
                     rule: "-j LIBVIRT_PRT".into(),
+                    comment: None,
                     counter: Counter {
                         packets: 607613,
                         bytes: 364557889,
                     },
+                }
+            )
+        );
+    }
+
+    #[test]
+    fn rule_parse_comment() {
+        assert_eq!(
+            Rule::parse_rule("[607613:364557889] -A POSTROUTING -j LIBVIRT_PRT -m comment --comment \"test comment\"").unwrap(),
+            (
+                "POSTROUTING".into(),
+                Rule {
+                    rule: "-j LIBVIRT_PRT -m comment --comment \"test comment\"".into(),
+                    comment: Some("test comment".into()),
+                    counter: Counter {
+                        packets: 607613,
+                        bytes: 364557889,
+                    },
+                }
+            )
+        );
+    }
+
+    #[test]
+    fn rule_parse_comment_escapes() {
+        assert_eq!(
+            Rule::parse_rule(
+                "[0:0] -A POSTROUTING -m comment --comment   \"test 'foo' \\\"comment\""
+            )
+            .unwrap(),
+            (
+                "POSTROUTING".into(),
+                Rule {
+                    rule: "-m comment --comment   \"test 'foo' \\\"comment\"".into(),
+                    comment: Some("test 'foo' \\\"comment".into()),
+                    counter: Counter::default()
+                }
+            )
+        );
+    }
+
+    #[test]
+    fn rule_parse_comment_no_quote() {
+        assert_eq!(
+            Rule::parse_rule("[0:0] -A POSTROUTING -m comment --comment   TEST-COMMENT").unwrap(),
+            (
+                "POSTROUTING".into(),
+                Rule {
+                    rule: "-m comment --comment   TEST-COMMENT".into(),
+                    comment: Some("TEST-COMMENT".into()),
+                    counter: Counter::default(),
                 }
             )
         );
